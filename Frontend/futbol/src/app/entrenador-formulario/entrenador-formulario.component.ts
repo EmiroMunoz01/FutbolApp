a7450783b0a4be7ede7ece1f3210bf3a8fceafb8
EmiroMunoz01 <emiro_2001@icloud.com>
2024-12-26 16:05:27 -0500

    se ha creado el componente del formulario

---
+++ Frontend/futbol/src/app/entrenador-formulario/entrenador-formulario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entrenador-formulario',
+  imports: [],
+  templateUrl: './entrenador-formulario.component.html',
+  styleUrl: './entrenador-formulario.component.css'
+})
+export class EntrenadorFormularioComponent {
+
+}
