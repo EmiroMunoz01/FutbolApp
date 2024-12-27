ae156128f8f6ba77642a467631786695416d0578
EmiroMunoz01 <emiro_2001@icloud.com>
2024-12-26 20:30:05 -0500

    Se ha creado la inyección de dependencias de formulario dentro del formulario se ha declarado una instancia del formulario en donde se van a designar los parámetros que se van a obtener de la vista del usuario las presentes modificaciones están haciendo en el archivo del componente. TS, adicional a eso se ha creado el método crear, el cual nos va a imprimir por consola los valores del formulario que tenemos esto se hace a modo de prueba. En los parámetros iniciales del formulario vamos a asignar a qué formulario pertenece para eso vamos a usar form group entre corchetes y le vamos a asignar el nombre del formulario que se ha designado en el componente. TS adicional a eso se le va a indicar el evento que se va a accionar al momento de hacer Clic esto lo hacemos por medio del método ngSubmiten donde se asigna el método crear que está dentro del componente .ts. Finalmente hemos asignado el nombre del input el cual se va a re direccionar la información que salga del punto a que es el formulario html hacia el punto b que es el componente type script donde tenemos la lógica de nuestra vista

--- Frontend/futbol/src/app/entrenador-formulario/entrenador-formulario.component.ts
+++ Frontend/futbol/src/app/entrenador-formulario/entrenador-formulario.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import {  RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-entrenador-formulario',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './entrenador-formulario.component.html',
-  styleUrl: './entrenador-formulario.component.css'
+  styleUrl: './entrenador-formulario.component.css',
 })
-
 export default class EntrenadorFormularioComponent {
+  //injectamos el formulario
+  private fb = inject(FormBuilder);
 
+
+  form = this.fb.group({
+    nombre: ['', [Validators.required]],
+    apellido: ['', [Validators.required]],
+    edad: ['', [Validators.required]],
+    cedula: ['', [Validators.required]],
+    nacionalidad: ['', [Validators.required]],
+  });
+
+  //
+
+  crear(){
+    console.log(this.form.value);
+  }
 }
