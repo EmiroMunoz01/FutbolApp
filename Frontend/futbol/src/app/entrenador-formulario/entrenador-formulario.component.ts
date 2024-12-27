322f168e48206efdc6eb09f085a242802e1a4b91
EmiroMunoz01 <emiro_2001@icloud.com>
2024-12-26 20:14:25 -0500

    Se han modificado las vistas del componente del formulario en donde se ha importado el módulo de router module para poder asignar la navegación de las rutas importante se debe agregar el tipo de elemento dentro del botón indicando que es un botón así suena redundante de lo contrario este no funcionará y solo se ejecutará fuera del formulario

--- Frontend/futbol/src/app/entrenador-formulario/entrenador-formulario.component.ts
+++ Frontend/futbol/src/app/entrenador-formulario/entrenador-formulario.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import {  RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-entrenador-formulario',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './entrenador-formulario.component.html',
   styleUrl: './entrenador-formulario.component.css'
 })
+
 export default class EntrenadorFormularioComponent {
 
 }
